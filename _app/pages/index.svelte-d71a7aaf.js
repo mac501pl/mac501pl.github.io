dedfd3bb56758e7cdc2ca2820ec0f0bfd5996fdc
mac501pl <mac501pl@users.noreply.github.com>
2022-04-09 20:55:21 +0000

    deploy: 6d43265737b0ef45a47b4754eab2480e58f1d1e6

---
+++ _app/pages/index.svelte-d71a7aaf.js
@@ -0,0 +1,8 @@
+import{S as u,i as d,s as m,e as c,g as h,a as n,B as v,c as o,C as i,f as l}from"../chunks/vendor-165455e6.js";function f(r){let e,a,s;return{c(){e=c("meta"),a=h(),s=c("main"),s.innerHTML=`<h1 class="svelte-pyc7ph"><span class="svelte-pyc7ph">Maciej \u017By\u0142ka</span>  <span class="hyphen svelte-pyc7ph">-</span>  <span class="svelte-pyc7ph">Full-stack developer</span></h1> 
+  <div class="svelte-pyc7ph"><img src="image.webp" width="400" height="400" alt="Maciej&#39;s picture" class="svelte-pyc7ph"/> 
+
+    <p class="svelte-pyc7ph">Full-stack developer, functional programming enthusiast. Currently working for
+      <a href="https://eda.sw.siemens.com/en-US/" target="_blank" rel="noreferrer noopener">Siemens EDA</a>. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Optio debitis animi doloribus, iure incidunt assumenda, eveniet
+      placeat nesciunt id architecto quod autem perferendis quo dolores beatae dolorum consequuntur obcaecati
+      repellendus?</p></div>`,document.title="Maciej \u017By\u0142ka",n(e,"name","description"),n(e,"content","This is the front page of the Maciej \u017By\u0142ka's personal site"),n(s,"class","svelte-pyc7ph")},m(t,p){v(document.head,e),o(t,a,p),o(t,s,p)},p:i,i,o:i,d(t){l(e),t&&l(a),t&&l(s)}}}class g extends u{constructor(e){super();d(this,e,null,f,m,{})}}export{g as default};
