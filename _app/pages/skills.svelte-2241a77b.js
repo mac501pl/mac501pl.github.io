eafd03b3fe9d6c01bae4057e17d2e54649e7f261
mac501pl <mac501pl@users.noreply.github.com>
2022-04-16 02:21:59 +0000

    deploy: 84f9fa299ddbb4c102dede65f7b4ff587d5b1172

---
+++ _app/pages/skills.svelte-2241a77b.js
@@ -0,0 +1,1 @@
+import{S as o,i,s as l,t as r,c,C as e,f}from"../chunks/vendor-99798518.js";function u(a){let t;return{c(){t=r("skills")},m(s,n){c(s,t,n)},p:e,i:e,o:e,d(s){s&&f(t)}}}class m extends o{constructor(t){super();i(this,t,null,u,l,{})}}export{m as default};
