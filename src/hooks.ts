31a44550f9a285e32fba33137060012a56b0f772
mac501pl <mzylka123@gmail.com>
2022-04-16 03:13:33 +0200

    fix hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -12,7 +12,7 @@ const options: Options = {
 export async function handle({ event, resolve }) {
   const response = await resolve(event);
 
-  if (response.headers.get('content-type').startsWith('text/html')) {
+  if (response.headers.get('content-type')?.startsWith('text/html')) {
     const body = await response.text();
     return new Response(await minify(body, options), response);
   }
