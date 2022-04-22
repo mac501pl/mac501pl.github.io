15b39fe8eaff6644755eaf25289689a1d5ee9649
mac501pl <mzylka123@gmail.com>
2022-04-23 01:19:54 +0200

    annoying

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { dev } from '$app/env';
 import { minify, type Options } from 'html-minifier-terser';
 import { parse, HTMLElement } from 'node-html-parser';
